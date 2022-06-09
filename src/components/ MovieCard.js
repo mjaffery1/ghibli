dad55ad47b5aa1bf4e3caa2619a0f167b34fe961
Mohammed <moejaffery@gmail.com>
2022-06-09 18:01:46 -0400

    ..

--- src/components/ MovieCard.js
+++ src/components/ MovieCard.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 function MovieCard({ movie }) {
-  console.log(movie);
   if (!movie) {
     return;
   }
--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -4,7 +4,6 @@ import { useParams } from "react-router-dom";
 function MovieDetails(props) {
   const { title } = useParams();
   const movie = props.data.find((mov) => mov.original_title === title);
-  // console.log(movie)
   return (
     <div className="details-container">
       <img
